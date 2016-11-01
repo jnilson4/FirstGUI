8a0606b2a392a1cb60b6c707aef9c32cd20ce437
Jake Nilson <jnil0572@740s-pl-132.local>
2016-11-01 12:51:07 -0600

    Added buttons to the panel!

--- src/graphics/view/FirstPanel.java
+++ src/graphics/view/FirstPanel.java
@@ -1,17 +1,23 @@
 package graphics.view;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
 import graphics.controller.GraphController;
 
 public class FirstPanel extends JPanel
 {
 	private GraphController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
 	
 	public FirstPanel(GraphController baseController)
 	{
 		super();
 		this.baseController = baseController;
 		
+		this.colorButton = new JButton("Colors!");
+		this.randomButton = new JButton("Randon Colors!!");
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -20,6 +26,9 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel()
 	{
+		this.setBackground(Color.DARK_GRAY);
+		this.add(colorButton);
+		this.add(randomButton);
 		
 	}
 	
